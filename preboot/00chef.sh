3fd104751be11faef0a9bb08629c2d0685655785
Karl Stoney <karl@jambr.co.uk>
2015-01-02 19:03:22 +0000

    Tidied up the logging

--- preboot/00chef.sh
+++ preboot/00chef.sh
@@ -8,29 +8,33 @@ chef_interval=${chef_interval:-"0"}
 chef_command="/usr/bin/chef-client -z -Fmin -c /etc/chef/client.rb -o $chef_run_list"
 chef_service_config="/etc/supervisord.d/chef-client.service.conf"
 chef_gce=${chef_gce:-"false"}
+
 cd /chef
 
 if [ "$chef_gce" == "false" ]; then
+  debug "GoogleCE disabled."
   echo "127.0.0.1 metadata.google.internal" >> /etc/hosts
 fi
 
 echo "node_name \"$chef_node_name\"" >> /etc/chef/client.rb
 
-if [ "$chef_run_list" == "" ]; then
-  echo " => WARNING: Chef run list was empty, skipping chef-client setup..."
-else
+if [ ! "$chef_run_list" == "" ]; then
   if [ "$chef_local" == "true" ]; then
-    echo " => Please wait... running chef in local mode..."
-    $chef_command
+    printf " => Please wait... Performing surface level configuration with chef-zero... "
+    if [ "$DEBUG" == "true" ]; then
+      $chef_command
+    else
+      $chef_command >/dev/null
+    fi
+    echo "Done!"
   fi
 
   if [ "$chef_interval" -gt "0" ]; then
-    echo " => Chef interval set at $chef_interval.  Enabling Supervisord chef service..."
-
+    debug "Chef interval set at $chef_interval.  Enabling Supervisord chef service..."
     echo "[program:chef-client]" > $chef_service_config
     echo "directory=/chef" >> $chef_service_config
     echo "command=$chef_command -i $chef_interval" >> $chef_service_config
   else
-    echo " => WARNING: chef_interval was 0, therefore chef-client will not run periodically!"
+    debug "WARNING: chef_interval was 0, therefore chef-client will not run periodically!"
   fi
 fi
